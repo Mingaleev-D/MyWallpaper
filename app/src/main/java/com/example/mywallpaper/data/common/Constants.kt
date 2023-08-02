e584d59086493673ba83d5bec81a90c51e4921c7
admin <mingaleev.d92@mail.ru>
2023-08-02 23:10:09 +0300

    3 функции для апп

--- app/src/main/java/com/example/mywallpaper/data/common/Constants.kt
+++ app/src/main/java/com/example/mywallpaper/data/common/Constants.kt
@@ -1,4 +1,13 @@
 package com.example.mywallpaper.data.common
 
 object Constants {
+   const val HOME = "Home"
+   const val POPULAR = "Popular"
+   const val RANDOM = "Random"
+   const val CATEGORY_NAME = "Categories"
+
+   object BackGroundState {
+      const val lockScreen = 2
+      const val backGround = 1
+   }
 }
